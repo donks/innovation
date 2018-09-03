0c3b7cb33b2b22009e8fb9af9a7df4f9232dd30d
Jason Donkin <donks@users.noreply.github.com>
2018-09-03 15:29:45 +1000

    remove
    remove

--- variables.tf
+++
@@ -1,9 +0,0 @@
-//
-// AS Azure Variables
-//
-
-# Instantiate login variables from environment variables sourced from terraform.tfvars
-variable "AZURE_SUBSCRIPTION_ID" {}
-variable "AZURE_CLIENT_ID" {}
-variable "AZURE_CLIENT_SECRET" {}
-variable "AZURE_TENANT_ID" {}
